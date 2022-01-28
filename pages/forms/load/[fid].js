8db3fcbd1998269f7cc5f69aa1d11a2c4ef3154e
merlinfuchs <merlinfuchs2001@gmail.com>
2022-01-28 22:46:54 +0100

    format channel list by categories

--- pages/forms/load/[fid].js
+++ pages/forms/load/[fid].js
@@ -1,8 +1,10 @@
 import {serverSideTranslations} from "next-i18next/serverSideTranslations";
 import apiRequest from "../../../api";
 import FormsLayout from "../../../layouts/forms";
-import {useState} from "react";
+import React, {useState} from "react";
 import {toast} from "react-toastify";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faChevronDown, faVolumeUp} from "@fortawesome/free-solid-svg-icons";
 
 export async function getServerSideProps({params, locale}) {
     const resp = await apiRequest({path: `/forms/${params.fid}`})
@@ -30,12 +32,95 @@ function roleColor({color}) {
     }
 }
 
+export function makeChannelList(channels, excluded, toggleExcluded) {
+    const result = []
+
+    const ChannelTypes = {
+        GUILD_TEXT: 0,
+        DM: 1,
+        GUILD_VOICE: 2,
+        GROUP_DM: 3,
+        GUILD_CATEGORY: 4,
+        GUILD_NEWS: 5,
+        GUILD_STORE: 6
+    };
+
+    const noParent = channels
+        .filter(c => !c.parent_id && c.type !== ChannelTypes.GUILD_CATEGORY)
+        .sort((a, b) => a.type > b.type ? 1 : a.type < b.type ? -1 : a.position > b.position)
+
+    for (let channel of noParent) {
+        if (channel.type === ChannelTypes.GUILD_VOICE) {
+            result.push(
+                <div className="pl-5 flex items-center" key={channel.id}>
+                    <input type="checkbox" checked={!excluded.includes(channel.id)}
+                           onChange={() => toggleExcluded(channel.id)}
+                           className="w-5 h-5 mr-2"/>
+                    <div>{channel.name}</div>
+                </div>
+            )
+        } else {
+            result.push(
+                <div className="pl-5 flex items-center" key={channel.id}>
+                    <input type="checkbox" checked={!excluded.includes(channel.id)}
+                           onChange={() => toggleExcluded(channel.id)}
+                           className="w-5 h-5 mr-2"/>
+                    <div className="align-middle name">{channel.name}</div>
+                </div>
+            )
+        }
+    }
+
+    const categories = channels
+        .filter(c => c.type === ChannelTypes.GUILD_CATEGORY)
+        .sort((a, b) => a.type > b.type)
+
+    for (let channel of categories) {
+        result.push(
+            <div className="uppercase font-bold pb-2 pt-4 flex items-center" key={channel.id}>
+                <input type="checkbox" checked={!excluded.includes(channel.id)}
+                           onChange={() => toggleExcluded(channel.id)}
+                           className="w-5 h-5 mr-2"/>
+                <div>{channel.name}</div>
+            </div>
+        )
+
+        const children = channels
+            .filter(c => c.parent_id && c.parent_id === channel.id)
+            .sort((a, b) => a.type > b.type ? 1 : a.type < b.type ? -1 : a.position > b.position)
+
+        for (let child of children) {
+            if (child.type === ChannelTypes.GUILD_VOICE) {
+                result.push(
+                    <div className="pl-5 flex items-center" key={child.id}>
+                        <input type="checkbox" checked={!excluded.includes(child.id)}
+                           onChange={() => toggleExcluded(child.id)}
+                           className="w-5 h-5 mr-2"/>
+                        <div>{child.name}</div>
+                    </div>
+                )
+            } else {
+                result.push(
+                    <div className="pl-5 flex items-center" key={child.id}>
+                        <input type="checkbox" checked={!excluded.includes(child.id)}
+                           onChange={() => toggleExcluded(child.id)}
+                           className="w-5 h-5 mr-2"/>
+                        <div className="align-middle name">{child.name}</div>
+                    </div>
+                )
+            }
+        }
+    }
+
+    return result
+}
+
 export default function AdvancedLoadOptionsForm({formId, form}) {
     const {guild, backup} = form.meta
     // guild.roles.sort((a, b) => a.position < b.position)
 
-    const [excludeDelete, setExcludeDelete] = useState([])
-    const [excludeLoad, setExcludeLoad] = useState([])
+    const [excludeDelete, setExcludeDelete] = useState(form.data?.exclude_delete_ids || [])
+    const [excludeLoad, setExcludeLoad] = useState(form.data?.exclude_load_ids || [])
 
     function toggleExcludeDelete(id) {
         if (excludeDelete.includes(id)) {
@@ -82,7 +167,7 @@ export default function AdvancedLoadOptionsForm({formId, form}) {
                     <div className="text-xl mb-3 text-gray-200">Delete the following:</div>
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                         <div className="bg-theme-dark p-3 rounded-md flex flex-col space-y-1">
-                            {guild.roles.map(role => (
+                            {guild.roles.filter(r => !r.managed && r.id !== guild.id).map(role => (
                                 <div key={role.id} className="flex space-x-2 items-center">
                                     <input type="checkbox" checked={!excludeDelete.includes(role.id)}
                                            onChange={() => toggleExcludeDelete(role.id)}
@@ -92,14 +177,7 @@ export default function AdvancedLoadOptionsForm({formId, form}) {
                             ))}
                         </div>
                         <div className="bg-theme-dark p-3 rounded-md flex flex-col space-y-1">
-                            {guild.channels.map(channel => (
-                                <div key={channel.id} className="flex space-x-2 items-center">
-                                    <input type="checkbox" checked={!excludeDelete.includes(channel.id)}
-                                           onChange={() => toggleExcludeDelete(channel.id)}
-                                           className="w-5 h-5"/>
-                                    <div>{channel.name}</div>
-                                </div>
-                            ))}
+                            {makeChannelList(guild.channels, excludeDelete, toggleExcludeDelete)}
                         </div>
                     </div>
                 </div>
@@ -117,14 +195,7 @@ export default function AdvancedLoadOptionsForm({formId, form}) {
                             ))}
                         </div>
                         <div className="bg-theme-dark p-3 rounded-md flex flex-col space-y-1">
-                            {backup.channels.map(channel => (
-                                <div key={channel.id} className="flex space-x-2 items-center">
-                                    <input type="checkbox" checked={!excludeLoad.includes(channel.id)}
-                                           onChange={() => toggleExcludeLoad(channel.id)}
-                                           className="w-5 h-5"/>
-                                    <div>{channel.name}</div>
-                                </div>
-                            ))}
+                            {makeChannelList(guild.channels, excludeLoad, toggleExcludeLoad)}
                         </div>
                     </div>
                 </div>
